ec569072d757d5466ad1aec120480ade1ce3342c
diglopes <diego-chez@hotmail.com>
2019-09-16 15:54:08 -0300

    feat: adds sass loader and change bootstrap colors

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,8 @@ module.exports = {
     */
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"] // <-- Run order
       }
     ]
   }
