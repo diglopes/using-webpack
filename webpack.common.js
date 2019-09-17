7f41a11dbed78bd2a10a55891292856bf8e02b74
diglopes <diego-chez@hotmail.com>
2019-09-17 13:00:21 -0300

    feat: extrat css into own file and minify html, css and js in production mode

--- webpack.common.js
+++ webpack.common.js
@@ -1,25 +1,15 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
 module.exports = {
   // The entry point file path
   entry: {
     main: "./src/index.js",
     vendor: "./src/vendor.js"
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html"
-    })
-  ],
+
   module: {
     /* Everytime the webpack came across a file that matches with the 
        regex it uses the loaders inserted into the array.
     */
     rules: [
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"] // <-- Run order
-      },
       {
         test: /\.html$/,
         use: ["html-loader"]
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const common = require("./webpack.common");
 const webpackMerge = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = webpackMerge(common, {
   mode: "development",
@@ -8,5 +9,18 @@ module.exports = webpackMerge(common, {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"] // <-- Run order
+      }
+    ]
   }
 });
